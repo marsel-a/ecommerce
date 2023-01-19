b1d3c058ec14c8d55e8298b0c1e571a453924207
marsel <marsel@getcraft.com>
2023-01-20 06:01:59 +0800

    Add drop-shadow

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const Home = () => {
         <title>Login Page</title>
       </Head>
       <main className="w-full bg-[url('/bg.svg')] bg-repeat bg-[length:600px_600px]">
-        <div className="prose flex flex-col items-center gap-4 justify-center h-screen bg-base-200 ml-auto">
+        <div className="prose flex flex-col items-center gap-4 justify-center h-screen bg-base-200 ml-auto drop-shadow-2xl">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-40">
             <path
               d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z"
